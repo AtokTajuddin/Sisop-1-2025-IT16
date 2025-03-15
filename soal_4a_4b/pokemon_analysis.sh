3c9a1b5de4e9c2f764f774dc653abf834dfa7d4b
rzkcp <syauqireza28@gmail.com>
2025-03-15 23:57:08 +0700

    add soal 4b

--- soal_4a_4b/pokemon_analysis.sh
+++ soal_4a_4b/pokemon_analysis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#!/bin/bash
+#a
 
 file="$1"
 
@@ -17,5 +17,30 @@ if [[ "$2" == "--info" ]]; then
 
     echo "Highest Adjusted Usage:  $highest_usage_pokemon with $highest_usage_value%"
     echo "Highest Raw Usage:       $highest_raw_pokemon with $highest_raw_value uses"
+fi
+
+#b
+
+if [[ "$2" == "--sort" ]]; then
+    column="$3"
+    
+    case "$column" in
+        "usage") sort_col=2 ;;
+        "raw") sort_col=3 ;;
+        "name") sort_col=1 ;;
+        "hp") sort_col=6 ;;
+        "atk") sort_col=7 ;;
+        "def") sort_col=8 ;;
+        "spatk") sort_col=9 ;;
+        "spdef") sort_col=10 ;;
+        "speed") sort_col=11 ;;
+        *) echo "Invalid column!"; exit 1 ;;
+    esac
+
+    if [[ "$column" == "name" ]]; then
+        sort -t, -k"$sort_col" "$file"
+    else
+        (head -n 1 "$file"; tail -n +2 "$file" | sort -t, -k"$sort_col" -nr)
+    fi
 fi
 
