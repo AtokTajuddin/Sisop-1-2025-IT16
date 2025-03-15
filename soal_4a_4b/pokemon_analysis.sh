162e683c8c2165c409f4e2d7a7f379f4027b847d
rzkcp <syauqireza28@gmail.com>
2025-03-15 23:53:12 +0700

    add soal 4a

--- soal_4a_4b/pokemon_analysis.sh
+++ soal_4a_4b/pokemon_analysis.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
+#!/bin/bash
+
+file="$1"
+
+if [[ "$2" == "--info" ]]; then
+    echo "Summary of $file"
+
+    highest_usage=$(tail -n +2 "$file" | sort -t, -k2 -nr | head -1)
+    highest_usage_pokemon=$(echo "$highest_usage" | cut -d, -f1)
+    highest_usage_value=$(echo "$highest_usage" | cut -d, -f2)
+
+    highest_raw=$(tail -n +2 "$file" | sort -t, -k3 -nr | head -1)
+    highest_raw_pokemon=$(echo "$highest_raw" | cut -d, -f1)
+    highest_raw_value=$(echo "$highest_raw" | cut -d, -f3)
+
+    echo "Highest Adjusted Usage:  $highest_usage_pokemon with $highest_usage_value%"
+    echo "Highest Raw Usage:       $highest_raw_pokemon with $highest_raw_value uses"
+fi
 
